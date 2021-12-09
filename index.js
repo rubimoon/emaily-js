472d3006da76dce884f4b273f697fa509794131d
Ruby Chang <dev.rubimoon@gmail.com>
2021-12-09 16:03:27 +0800

    configure db

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 require('./services/passport');
 require('./routes/authRoutes')(app);
 
 // in production, the PORT will be provided by heroku
 const PORT = process.env.PORT || 5050;
+const CONNECTION_STRING = keys.mongoURL;
+
 // express telling node to listen to the port.
+mongoose.connect(CONNECTION_STRING);
 app.listen(PORT);
