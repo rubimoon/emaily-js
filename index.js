4b8fe67411cb1beee92d3b0db2983c3623d5d5f9
Ruby Chang <dev.rubimoon@gmail.com>
2021-12-09 14:38:09 +0800

    refactor index.js

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
-
 const app = express();
 
-// kinda like generic Register
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-    },
-    (accessToken, refreshToken, profile) => {
-      console.log('access token:' + accessToken);
-      console.log('refresh token:' + refreshToken);
-      console.log(profile);
-    }
-  )
-);
-
-//the first time to grant permission
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./services/passport');
+require('./routes/authRoutes')(app);
 
 // in production, the PORT will be provided by heroku
 const PORT = process.env.PORT || 5050;
