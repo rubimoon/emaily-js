ae984c0100ae42d13ca39c1909f96e6208abcebb
Ruby Chang <dev.rubimoon@gmail.com>
2021-12-10 16:38:35 +0800

    add link to sign up with google

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href='http://localhost:5050/auth/google'>Sign in with Google</a>
       </header>
     </div>
   );
