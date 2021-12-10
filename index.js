e1ca6cf5990e4349083709a4985f6c77d50c4fd7
Ruby Chang <dev.rubimoon@gmail.com>
2021-12-10 13:10:52 +0800

    fix the order

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const keys = require('./config/keys');
 const express = require('express');
-
-const app = express();
 const mongoose = require('mongoose');
+const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 require('./models/User');
 require('./services/passport');
 
-const PORT = process.env.PORT || 5050;
 // express telling node to listen to the port.
 mongoose.connect(keys.mongoURL);
-app.use(passport.initialize());
-app.use(passport.session());
+
+const app = express();
+
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey],
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
-
+const PORT = process.env.PORT || 5050;
 // in production, the PORT will be provided by heroku
 app.listen(PORT);
