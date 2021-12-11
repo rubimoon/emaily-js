ce5d3786782ef4e0564c31e1bc7567d83b6477a7
Ruby Chang <dev.rubimoon@gmail.com>
2021-12-11 15:13:56 +0800

    import css module

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Landing = () => <h2>Landing</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path='/' component={Landing} />
+        <Route  path='/surveys' component={SurveyNew} />
+
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
