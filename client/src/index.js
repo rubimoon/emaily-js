7ec0b6cef239eda9f58180998d416b7b0019d6a3
Ruby Chang <dev.rubimoon@gmail.com>
2021-12-11 11:37:10 +0800

    import depencies and implement bootup logic

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+const store = createStore(() => [], {}, applyMiddleware);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
+
+
