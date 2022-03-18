e1d51e1fa6ac10da9f7383481197bafc4d48c4a7
Ruby Chang <dev.rubimoon@gmail.com>
2022-03-18 14:29:24 +0800

    add some style

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
-
+import { connect } from 'react-redux';
 import Routes from './Routes';
 import * as actions from '../state/actions';
 
 const App = () => {
   return (
-    <>
+    <div className='container'>
       <Header />
       <Routes />
-    </>
+    </div>
   );
 };
 
