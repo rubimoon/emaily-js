f08ce324c8e939f6596d2359c775daa4806b6038
Ruby Chang <dev.rubimoon@gmail.com>
2022-03-18 13:55:44 +0800

    add some layout

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Header from '../components/Header';
+import { connect } from 'react-redux';
+import * as actions from '../state/actions';
 
 const Landing = () => <h2>Landing</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 
-
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Route exact path='/' component={Landing} />
-        <Route  path='/surveys' component={SurveyNew} />
-
-      </div>
-    </BrowserRouter>
+    <>
+      <Header />
+      <Route exact path='/' component={Landing} />
+      <Route path='/surveys' component={SurveyNew} />
+    </>
   );
 };
 
-export default App;
+export default connect()(App);
---
+++ client/src/components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <nav>
+      <div className='nav-wrapper'>
+        <a className='left brand-logo'>Emaily</a>
+        <ul className='right'>
+          <li>
+            <a>Login with Google</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
