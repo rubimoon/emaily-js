70ba6c3e366d4f437831ea5c4f4729fcb0fa0bc9
Ruby Chang <dev.rubimoon@gmail.com>
2022-07-24 20:39:54 +0900

    chore

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5050;
 
 // Services
-require('./services/auth');
+require("./services/auth");
 // Models
-require('./models/User');
-require('./models/Survey');
+require("./models/User");
+require("./models/Survey");
 
 // Db connection
-require('./services/database/persistence');
-require('./services/database/cache');
+require("./services/database/persistence");
+require("./services/database/cache");
 
 // Express Server
 const app = express();
 app.use(bodyParser.json());
 
 // Middlewares
-require('./middlewares/auth')(app);
-require('./routes/authRoutes')(app);
-require('./routes/paymentRoutes')(app);
-require('./routes/surveyRoutes')(app);
+require("./middlewares/auth")(app);
+require("./routes/authRoutes")(app);
+require("./routes/paymentRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
